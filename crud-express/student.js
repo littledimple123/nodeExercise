47d37ea27a82ff447e1c8e3795f0d2a94e2b4a33
lutiantian <cztian1314@163.com>
2018-12-03 17:37:50 +0800

    sencond

--- crud-express/student.js
+++ crud-express/student.js
@@ -5,7 +5,22 @@ mongoose.connect('mongodb://localhost/itstudent')
 var Schema = mongoose.Schema
 
 var commentSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: Number,
+        enum: [0, 1]
+    },
+    age: {
+        type: Number
+    },
+    hobbies {
+        type: String
+    }
 
 })
 
+//直接导出模型构造函数
 module.exports = mongoose.model('Student', commentSchema)
